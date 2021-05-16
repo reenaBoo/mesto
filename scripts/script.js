235100482119d97e8db8fa88c8efefb5b60ca360
Karina Lyapina <ivettra@yandex.ru>
2021-05-16 14:18:04 +0500

    добавить весь функционал формы

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,36 @@
-let popup = document.querySelector('.popup'); //секция с попапом
+const popup = document.querySelector('.popup'); //секция с попапом
 let nameInput = popup.querySelector('.form__input_type_name'); //поле формы с именем пользователя
 let jobInput = popup.querySelector('.form__input_type_job'); //поле формы с описание работы пользователя
-let closeButton = popup.querySelector('.popup__close-button'); //кнопка закрытия формы
-let profile = document.querySelector('.profile'); //секция с профилем
+const closeButton = popup.querySelector('.popup__close-button'); //кнопка закрытия формы
+const profile = document.querySelector('.profile'); //секция с профилем
 let userName = profile.querySelector('.profile-info__name'); //имя пользователя
 let userJob = profile.querySelector('.profile-info__description'); //описание работы пользователя
-let editButton = profile.querySelector('.profile-info__edit-button'); //кнопка редактирования формы
+const editButton = profile.querySelector('.profile-info__edit-button'); //кнопка редактирования формы
 
-//добавляем событие при клике на кнопку редактирования профиля
-editButton.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
-});
+nameInput.value = userName.textContent; //данные берутся из профиля
+jobInput.value = userJob.textContent; //данные берутся из профиля
 
-//убираем класс при закрытии формы
-function closePopup() {
-  popup.classList.remove('popup_opened');
+//функция для переключения класса при открытии и закрытии формы
+function toggleClass() {
+  popup.classList.toggle('popup_opened');
 };
 
-//сохраняем внесенные изменения в форму
+//функция для закрытия формы при нажатии на оверлей
+function overlayClick(event) {
+  if (event.target === event.currentTarget) {
+    toggleClass()
+  }
+};
+
+//функция для сохранения внесенных в форму изменений
 function formSubmitHandler (evt) {
   evt.preventDefault();
   userName.textContent = nameInput.value;
   userJob.textContent = jobInput.value;
+  toggleClass()
+};
 
-  closePopup()
-}
-
-popup.addEventListener('submit', formSubmitHandler); 
-closeButton.addEventListener('click', closePopup);
+popup.addEventListener('submit', formSubmitHandler); //сохраняем изменения в профиле
+closeButton.addEventListener('click', toggleClass); //закрываем форму при нажатии на крестик
+popup.addEventListener('click', overlayClick); //закрываем форму при нажатии на оверлей
+editButton.addEventListener('click', toggleClass); //открываем форму при нажатии кнопку редактирования профиля
