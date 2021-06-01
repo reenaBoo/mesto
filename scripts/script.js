b5ed255f1b526df599f9f5c7ad1dd922d155b703
Karina Lyapina <ivettra@yandex.ru>
2021-06-01 21:20:44 +0500

    fix: объявить переменные через const

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
 const popupProfile = document.querySelector('.popup_type_edit');
-let nameInput = popupProfile.querySelector('.form__input_type_name');
-let jobInput = popupProfile.querySelector('.form__input_type_job');
+const nameInput = popupProfile.querySelector('.form__input_type_name');
+const jobInput = popupProfile.querySelector('.form__input_type_job');
 const popupProfileCloseButton = popupProfile.querySelector('.popup__close-button');
 //-----------------------------------------------------------------------------
 const profile = document.querySelector('.profile'); //секция с профилем
-let userName = profile.querySelector('.profile__name'); //имя пользователя
-let userJob = profile.querySelector('.profile__description'); //описание работы пользователя
+const userName = profile.querySelector('.profile__name'); //имя пользователя
+const userJob = profile.querySelector('.profile__description'); //описание работы пользователя
 const editButton = profile.querySelector('.profile__edit-button'); //кнопка редактирования формы
 const addCardButton = profile.querySelector('.profile__add-button');
 //-----------------------------------------------------------------------------
 const popupNewCard = document.querySelector('.popup_type_new-card');
-let mestoName = popupNewCard.querySelector('.form__input_type_mesto');
-let mestoLink = popupNewCard.querySelector('.form__input_type_url');
+const mestoName = popupNewCard.querySelector('.form__input_type_mesto');
+const mestoLink = popupNewCard.querySelector('.form__input_type_url');
 const formNewCard = popupNewCard.querySelector('.form');
 const popupNewCardCloseButton = popupNewCard.querySelector('.popup__close-button');
 const cards = document.querySelector('.cards');
 //-----------------------------------------------------------------------------
 const popupImage = document.querySelector('.popup_type_image');
-let figureImage = popupImage.querySelector('.figure__image');
-let figureTitle = popupImage.querySelector('.figure__title');
+const figureImage = popupImage.querySelector('.figure__image');
+const figureTitle = popupImage.querySelector('.figure__title');
 const popupImageCloseButton = popupImage.querySelector('.popup__close-button');
 
 function openPopup(popup) {
@@ -133,8 +133,8 @@ initialCards.forEach((item) => {
 function addCardSubmitHandler(evt) {
   evt.preventDefault();
   
-  let newCardName = mestoName.value;
-  let newCardLink = mestoLink.value;
+  const newCardName = mestoName.value;
+  const newCardLink = mestoLink.value;
   
   const addCardMass = {
     name: newCardName,
