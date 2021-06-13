4606549b5be93950409e22e1d4da04fd4d7d37ef
Karina Lyapina <ivettra@yandex.ru>
2021-06-13 23:19:10 +0500

    feat: создать объект с классами, исправить класс у span, создать классы для текста ошибки

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+enableValidation({
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__save-button',
+  inactiveButtonClass: 'form__save-button_disabled',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_active'
+}); 
