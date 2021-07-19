3408eb784e3fdee1a0927cd577762c6f4525527d
Karina Lyapina <ivettra@yandex.ru>
2021-07-19 22:02:43 +0500

    коммит

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import {Card} from './Card.js';
-import {FormValidator} from './FormValidator.js';
-import {Section} from './Section.js';
-import {PopupWithImage} from './PopupWithImage.js';
-import {PopupWithForm} from './PopupWithForm.js';
-import {UserInfo} from './UserInfo.js';
-import {name, url, initialCards, obj, userForm, cardForm, editButton, addCardButton, userName, userJob} from './constants.js';
+import {Card} from '../components/Card.js';
+import {FormValidator} from '../components/FormValidator.js';
+import {Section} from '../components/Section.js';
+import {PopupWithImage} from '../components/PopupWithImage.js';
+import {PopupWithForm} from '../components/PopupWithForm.js';
+import {UserInfo} from '../components/UserInfo.js';
+import {name, url, initialCards, obj, userForm, cardForm, editButton, addCardButton, userName, userJob} from '../utils/constants.js';
+import './index.css';
 
 //---------------рендер карточек--------------------------------
 const renderCards = new Section({
